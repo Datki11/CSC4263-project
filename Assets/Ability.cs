9a1dfa5cd73e898b4ad649a23fb573599dfacce7
Taylor Mathews <tmathews@crimer.com>
2020-03-12 13:29:04 -0500

    Created some initial classes

---
+++ Assets/Ability.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ability
+{
+    string Name;
+    string Description;
+
+    Character User;
+    Character Target;
+
+    float powerRange;
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class Player : Character
+{
+	#region Public Properties
+	public override ICharClass Class
+	{
+		get;
+		set;
+	}
+	public override int MaxHealth
+	{
+		get;
+		set;
+	}
+	public override int CurrentHealth
+	{
+		get;
+		set;
+	}
+	public override int Level
+	{
+		get;
+		set;
+	}
+	public override int CurrentExp
+	{
+		get;
+		set;
+	}
+
+	//possibly wrap below stats, maybe health too, in a Stats class
+	public override int Attack
+	{
+		get;
+		set;
+	}
+	public override int Defense
+	{
+		get;
+		set;
+	}
+	public override int Speed
+	{
+		get;
+		set;
+	}
+	#endregion
+
+	#region Methods
+	public override void LevelUp()
+    {
+		base.LevelUp();
+    }
+	#endregion
+
+	#region MonoBehaviour
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    #endregion
+}
