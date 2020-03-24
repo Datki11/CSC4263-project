a20ab2bb570596add459cfe964424c9ee8c02e3c
Daniel Atkinson <datkinson@crimer.com>
2020-03-24 14:11:07 -0500

    Added animation to enemy attacks

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -41,6 +41,8 @@ public abstract class Enemy : Unit
 		render = GetComponent<SpriteRenderer>();
 	}
 
+	private float startX;
+
 	public virtual void Act() {
 
 		//Default implementation is a completely random attack
@@ -48,6 +50,29 @@ public abstract class Enemy : Unit
         int characterNum = Mathf.RoundToInt(Random.Range(0, characters.Count - 1));
         int abilityNum = Mathf.RoundToInt(Random.Range(0, Abilities.Count - 1));
         Abilities[abilityNum].Action(characters[characterNum].GetComponent<Unit>());
+		AttackAnimate();
+	}
+	private void AttackAnimate() {
+		startX = transform.position.x;
+		GoRight();
+	}
+	private void GoRight() {
+		transform.Translate(5 * Time.deltaTime, 0f, 0f);
+		if (transform.position.x - startX >= 1) {
+			GoLeft();
+		}
+		else {
+			Invoke("GoRight", 0.01f);
+		}
+	}
+	private void GoLeft() {
+		transform.Translate(-5 * Time.deltaTime, 0f, 0f);
+		if (transform.position.x - startX <= 0) {
+			transform.position = new Vector3(startX,transform.position.y,transform.position.z);
+		}
+		else {
+			Invoke("GoLeft", 0.01f);
+		}
 	}
 
 	public void Kill() {
